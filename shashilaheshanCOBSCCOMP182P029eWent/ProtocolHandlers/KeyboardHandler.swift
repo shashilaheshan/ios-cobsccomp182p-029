db581ef80ce9cdf5c98dd1a33f64eb0b29bfc6c9
Shashila Heshan <shahshila.heshan@thebetmakers.com>
2020-02-09 02:02:01 +0530

    added custom fonts and lotty animations

---
+++ shashilaheshanCOBSCCOMP182P029eWent/ProtocolHandlers/KeyboardHandler.swift
@@ -0,0 +1,9 @@
+//
+//  KeyboardHandler.swift
+//  shashilaheshanCOBSCCOMP182P029eWent
+//
+//  Created by Shashila Heshan on 8/2/20.
+//  Copyright © 2020 Shashila Heshan. All rights reserved.
+//
+
+import Foundation
---
+++ shashilaheshanCOBSCCOMP182P029eWent/UIExtentions/UiElementsExtentions/UITextFieldExtention.swift
@@ -0,0 +1,9 @@
+//
+//  UITextFieldExtention.swift
+//  shashilaheshanCOBSCCOMP182P029eWent
+//
+//  Created by Shashila Heshan on 8/2/20.
+//  Copyright © 2020 Shashila Heshan. All rights reserved.
+//
+
+import Foundation
